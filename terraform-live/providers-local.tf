f969ed03dba100b9b6ed977054177ee2f006a6a1
Julian Peißker <30857628+JPEasier@users.noreply.github.com>
2023-02-02 18:32:19 +0100

    delete local file

--- terraform-live/providers-local.tf
+++
@@ -1,9 +0,0 @@
-provider "azurerm" {
-  skip_provider_registration = true
-  subscription_id            = "xxxx"
-  tenant_id                  = "xxxx"
-
-  client_id     = "xxxx"
-  client_secret = "xxxx"
-  features {}
-}
